380acc9a85ae503a8a10bd9100faa1f15b5bfeaa
kieran <kieran.boudouin@epitech.eu>
2023-05-16 12:11:26 +0200

    reddit auth

--- components/ProfileScreen.js
+++ components/ProfileScreen.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { View, Text } from "react-native";
+import RedditLogin from "./RedditAuth"
 
 function ProfileScreen() {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>This is the profile screen</Text>
+      <RedditLogin/>
     </View>
   );
 }
