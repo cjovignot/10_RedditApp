7b912c9f96a67e43f4a789a422c47ad6cd165eb9
Cosme Jovignot <cosme.jovignot@epitech.eu>
2023-05-17 15:04:14 +0200

    [ADD] Videos

--- components/Card.js
+++ components/Card.js
@@ -8,10 +8,15 @@ import Icon from '@mdi/react';
 import { mdiChevronDoubleDown } from '@mdi/js';
 import { mdiChevronDoubleUp } from '@mdi/js';
 
+import { Video, ResizeMode } from 'expo-av';
+
 const LeftContent = props => <Avatar.Icon {...props} icon="folder" />
 
 const MyComponent = ({ subredditsData }) => {
   const { width } = useWindowDimensions();
+
+  const video = React.useRef(null);
+  const [status, setStatus] = React.useState({});
 
   // console.log(decode(subredditsData.data.media_metadata?.p?.u))
 
@@ -28,7 +33,7 @@ const MyComponent = ({ subredditsData }) => {
         <Text variant="titleSmall"style={[{color: 'grey'}, {fontStyle: 'italic'}]}>{subredditsData.data.subreddit_name_prefixed}</Text>
         <Text variant="titleLarge" numberOfLines={2}  style={[{fontWeight: 'bold'}]}>{subredditsData.data.title}</Text>
         
-        {subredditsData.data.preview &&
+        {subredditsData.data.preview && subredditsData.data.is_video === false &&
           <Card.Cover style={[{height: 300}, {width: 'auto'}, {marginTop: 20}]} source={{uri: decode(subredditsData.data.preview?.images[0]?.source?.url)}}/>
         }
 
@@ -39,11 +44,21 @@ const MyComponent = ({ subredditsData }) => {
         {subredditsData.data.is_gallery && subredditsData.data.is_gallery === true && (
           <Card.Cover source={{ uri: decode(subredditsData.data.media_metadata[Object.keys(subredditsData.data.media_metadata)[0]].p[3].u) }}/>
         )}
-        
-        {subredditsData.data.is_gallery && subredditsData.data.is_gallery === true && (
-          <Card.Cover source={{ uri: decode(subredditsData.data.media_metadata[Object.keys(subredditsData.data.media_metadata)[0]].p[3].u) }}/>
-        )}
 
+        {subredditsData.data.is_video && subredditsData.data.is_video === true && (
+          <Video
+            ref={video}
+            style={styles.video}
+            source={{
+              uri: subredditsData.data.secure_media.reddit_video.fallback_url,
+            }}
+            useNativeControls
+            resizeMode={ResizeMode.CONTAIN}
+            isLooping
+            isMuted={false}
+            onPlaybackStatusUpdate={status => setStatus(() => status)}
+          />
+        )}
 
 
 
@@ -64,9 +79,13 @@ const MyComponent = ({ subredditsData }) => {
 
 const styles = StyleSheet.create({
     card: {
-        marginTop: 10,
-        borderRadius: 4,
+      marginTop: 10,
+      borderRadius: 4,
     },
+    video: {
+      height: 300,
+      width: 350,
+    }
 });
 
 export default MyComponent;
