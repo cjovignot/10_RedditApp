abbf4638ef37b91875223a6a291c87dee6c3b18d
arthur <arthur.asselin@epitech.eu>
2023-05-19 13:26:31 +0200

    fixed upvote

--- components/HomePage.js
+++ components/HomePage.js
@@ -1,12 +1,11 @@
 import React, { useEffect } from "react";
 import { View, Text, Image, FlatList, StyleSheet } from "react-native";
-import Card from './Card';
-import Filters from './Filters';
-import axios from 'axios';
+import Card from "./Card";
+import Filters from "./Filters";
+import axios from "axios";
 
 function HomePage() {
   const [subredditsData, setSubredditsData] = React.useState([]);
-  const [newSubredditsData, setNewSubredditsData] = React.useState([]);
   const [after, setAfter] = React.useState(null);
 
   const [best, setBest] = React.useState(true);
@@ -16,132 +15,63 @@ function HomePage() {
   const handleBestChange = (value) => {
     setBest(value);
   };
+
   const handleHotChange = (value) => {
     setHot(value);
   };
+
   const handleNewsChange = (value) => {
     setNews(value);
   };
 
+  const fetchData = (after = null) => {
+    // Fetch data based on the selected filter
+    const token =
+      "eyJhbGciOiJSUzI1NiIsImtpZCI6IlNIQTI1NjphVXJUQUUrdnZWVTl4K0VMWFNGWEcrNk5WS1FlbEdtSjlWMkQxcWlCZ3VnIiwidHlwIjoiSldUIn0.eyJzdWIiOiJ1c2VyIiwiZXhwIjoxNjg0NTY4OTQ1Ljc2NDAzMywiaWF0IjoxNjg0NDgyNTQ1Ljc2NDAzMiwianRpIjoiMzAyODI3NDgzMjM4LUNKYVhYZ0FsYkpiekJrR1lsMGdoYTItM1V1bVNtUSIsImNpZCI6IkxtS1kyQmJGSnNwaDNmZ29oZXUwTXciLCJsaWQiOiJ0Ml8zdjQ3bnhpZSIsImFpZCI6InQyXzN2NDdueGllIiwibGNhIjoxNTU5Mjc3MDQyMTE3LCJzY3AiOiJlSnlLVnRKU2lnVUVBQURfX3dOekFTYyIsImZsbyI6OX0.mhCVuz_k56qUlB5iaXVTEMy76ba6jwxlnnOaJVwVfdvd_cI8Cd7UbK_LdksPhUBlWswJV-rgowPHTLvATAnCXerhHyX-oFW8wNwrjVVvKuhMTwjFCJpLv3rB40gz7oBfqgqiShmwmyf54koET8GzY7aqZeb6fIXFjPmjXrVZl1iG3iXFjNtyz7jG0dbjlyflfQzs7dR2QfIuywmx0LahZyEkXwIoZd9NnFSVbVzfxwEh2LtKp1nN4PNq0QlukcM7AbQeCQEVQQU7svcUvj_VzNp9_FUWGo-p-b3NDT36Pfj4FMM_jIdocXdEZ_Xq1VFp43kqWYKVy95AHX-cttRwOQ";
+
+    const options = {
+      method: "GET",
+      url: best
+        ? "https://oauth.reddit.com/best"
+        : hot
+        ? "https://oauth.reddit.com/hot"
+        : "https://oauth.reddit.com/new",
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+    };
+
+    if (after) {
+      options.url += `?after=${after}`;
+    }
+
+    axios
+      .request(options)
+      .then(function (res) {
+        const data = res.data.data.children;
+        const newAfter = res.data.data.after;
+        setSubredditsData((prevData) => [...prevData, ...data]);
+        setAfter(newAfter);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
 
   useEffect(() => {
     // Reset the existing data when the filter value changes
     setSubredditsData([]);
-    setNewSubredditsData([]);
     setAfter(null);
 
     // Fetch new data based on the selected filter
-    if (best === true) {
-      console.log(subredditsData, "in best")
-      console.log(newSubredditsData, "new in best")
-      console.log("best fetch", best);
-      console.log("best ok");
-      const fetchData = (after = null) => {
-        const token = 'eyJhbGciOiJSUzI1NiIsImtpZCI6IlNIQTI1NjphVXJUQUUrdnZWVTl4K0VMWFNGWEcrNk5WS1FlbEdtSjlWMkQxcWlCZ3VnIiwidHlwIjoiSldUIn0.eyJzdWIiOiJ1c2VyIiwiZXhwIjoxNjg0NDkxMjA0LCJpYXQiOjE2ODQ0MDQ4MDQsImp0aSI6IjMxOTc5NTQ5MTc4OTI5LVJ6N1lqVzZuUENFZUFydUF2Y3EyelFqU3FYQjRtUSIsImNpZCI6ImlBanJEc0x5Rl9FU3RHSWFONDVOZ1EiLCJsaWQiOiJ0Ml9iYzM3OXc2NXQiLCJhaWQiOiJ0Ml9iYzM3OXc2NXQiLCJsY2EiOjE2ODQxNDg3ODc0MjAsInNjcCI6ImVKeUtWdEpTaWdVRUFBRF9fd056QVNjIiwiZmxvIjo5fQ.lz6ChgLuUy8MxKl47CxUrYJpetuJosJX-U58t9Yij0NkZH4qPyMjs5i0ux9YvsovQ48w0WMjcbQuUK_rYiT_1cx5ePCkqN3Ev1r1O-tzbMxMwUXEGq2yQrXq4PWlIgjIpvTPs2hlFlRa82HOda_d5EipgZLkBWgJXzBWCcpYi4C0M_GccclDwlIfxhTn4Ewh64ZtmZ4o_SuXIkFQMHatsR55x-vXKCuYRMlmj6Lw3CvNt-pboECgRC7mrFdnOzXcFPMyHpHMwtQU9ak0qk3TjbYXYzyiixNabLet0tSexfARzvfMo0gIkIvsyNVNuLG2PGRAAGNPhxWZX_bDVxxeDw';
-
-        const options = {
-          method: 'GET',
-          url: 'https://oauth.reddit.com/best',
-          headers: {
-              Authorization: "Bearer " + token,
-          }
-        }
-
-        if (after) {
-          options.url += `?after=${after}`;
-        }
-          
-        axios.request(options).then(function (res) {
-          const data = res.data.data.children;
-          const newAfter = res.data.data.after;
-          const newSubredditsData = [...subredditsData, ...data];
-          console.log(newSubredditsData, "new in best after set")
-          setSubredditsData(newSubredditsData);
-          // console.log(newSubredditsData, "BEST DATA")
-          // setImageUrl(newSubredditsData.map(item => item.data));
-          setAfter(newAfter);
-        }).catch(function (error) {
-          console.error(error);
-        });
-      };
-      fetchData();
-
-    } else if (hot === true) {
-      console.log(subredditsData, "in hot")
-      console.log(newSubredditsData, "new in hot")
-      console.log("hot fetch", hot);
-      console.log("hot ok");
-      const fetchHot = (after = null) => {
-        const token = 'eyJhbGciOiJSUzI1NiIsImtpZCI6IlNIQTI1NjphVXJUQUUrdnZWVTl4K0VMWFNGWEcrNk5WS1FlbEdtSjlWMkQxcWlCZ3VnIiwidHlwIjoiSldUIn0.eyJzdWIiOiJ1c2VyIiwiZXhwIjoxNjg0NDkxMjA0LCJpYXQiOjE2ODQ0MDQ4MDQsImp0aSI6IjMxOTc5NTQ5MTc4OTI5LVJ6N1lqVzZuUENFZUFydUF2Y3EyelFqU3FYQjRtUSIsImNpZCI6ImlBanJEc0x5Rl9FU3RHSWFONDVOZ1EiLCJsaWQiOiJ0Ml9iYzM3OXc2NXQiLCJhaWQiOiJ0Ml9iYzM3OXc2NXQiLCJsY2EiOjE2ODQxNDg3ODc0MjAsInNjcCI6ImVKeUtWdEpTaWdVRUFBRF9fd056QVNjIiwiZmxvIjo5fQ.lz6ChgLuUy8MxKl47CxUrYJpetuJosJX-U58t9Yij0NkZH4qPyMjs5i0ux9YvsovQ48w0WMjcbQuUK_rYiT_1cx5ePCkqN3Ev1r1O-tzbMxMwUXEGq2yQrXq4PWlIgjIpvTPs2hlFlRa82HOda_d5EipgZLkBWgJXzBWCcpYi4C0M_GccclDwlIfxhTn4Ewh64ZtmZ4o_SuXIkFQMHatsR55x-vXKCuYRMlmj6Lw3CvNt-pboECgRC7mrFdnOzXcFPMyHpHMwtQU9ak0qk3TjbYXYzyiixNabLet0tSexfARzvfMo0gIkIvsyNVNuLG2PGRAAGNPhxWZX_bDVxxeDw';
-
-        const options = {
-          method: 'GET',
-          url: 'https://oauth.reddit.com/hot',
-          headers: {
-              Authorization: "Bearer " + token,
-          }
-        }
-
-        if (after) {
-          options.url += `?after=${after}`;
-        }
-          
-        axios.request(options).then(function (res) {
-          const data = res.data.data.children;
-          const newAfter = res.data.data.after;
-          const newSubredditsData = [...subredditsData, ...data];
-          console.log(newSubredditsData, "new in hot after set")
-          setSubredditsData(newSubredditsData);
-          // console.log(newSubredditsData, "HOT DATA")
-          // setImageUrl(newSubredditsData.map(item => item.data));
-          setAfter(newAfter);
-        }).catch(function (error) {
-          console.error(error);
-        });
-      };
-      fetchHot();
-    } else if (news === true) {
-      console.log(subredditsData, "in news")
-      console.log(newSubredditsData, "new in news")
-      console.log("news fetch", news);
-      console.log("news ok");
-      const fetchNews = (after = null) => {
-        const token = 'eyJhbGciOiJSUzI1NiIsImtpZCI6IlNIQTI1NjphVXJUQUUrdnZWVTl4K0VMWFNGWEcrNk5WS1FlbEdtSjlWMkQxcWlCZ3VnIiwidHlwIjoiSldUIn0.eyJzdWIiOiJ1c2VyIiwiZXhwIjoxNjg0NDkxMjA0LCJpYXQiOjE2ODQ0MDQ4MDQsImp0aSI6IjMxOTc5NTQ5MTc4OTI5LVJ6N1lqVzZuUENFZUFydUF2Y3EyelFqU3FYQjRtUSIsImNpZCI6ImlBanJEc0x5Rl9FU3RHSWFONDVOZ1EiLCJsaWQiOiJ0Ml9iYzM3OXc2NXQiLCJhaWQiOiJ0Ml9iYzM3OXc2NXQiLCJsY2EiOjE2ODQxNDg3ODc0MjAsInNjcCI6ImVKeUtWdEpTaWdVRUFBRF9fd056QVNjIiwiZmxvIjo5fQ.lz6ChgLuUy8MxKl47CxUrYJpetuJosJX-U58t9Yij0NkZH4qPyMjs5i0ux9YvsovQ48w0WMjcbQuUK_rYiT_1cx5ePCkqN3Ev1r1O-tzbMxMwUXEGq2yQrXq4PWlIgjIpvTPs2hlFlRa82HOda_d5EipgZLkBWgJXzBWCcpYi4C0M_GccclDwlIfxhTn4Ewh64ZtmZ4o_SuXIkFQMHatsR55x-vXKCuYRMlmj6Lw3CvNt-pboECgRC7mrFdnOzXcFPMyHpHMwtQU9ak0qk3TjbYXYzyiixNabLet0tSexfARzvfMo0gIkIvsyNVNuLG2PGRAAGNPhxWZX_bDVxxeDw';
-
-        const options = {
-          method: 'GET',
-          url: 'https://oauth.reddit.com/new',
-          headers: {
-              Authorization: "Bearer " + token,
-          }
-        }
-
-        if (after) {
-          options.url += `?after=${after}`;
-        }
-          
-        axios.request(options).then(function (res) {
-          const data = res.data.data.children;
-          const newAfter = res.data.data.after;
-          const newSubredditsData = [...subredditsData, ...data];
-          console.log(newSubredditsData, "new in news after set")
-          setSubredditsData(newSubredditsData);
-          // console.log(newSubredditsData, "NEWs DATA")
-          // setImageUrl(newSubredditsData.map(item => item.data));
-          setAfter(newAfter);
-        }).catch(function (error) {
-          console.error(error);
-        });
-      };
-      fetchNews();
-    }
+    fetchData();
   }, [best, hot, news]);
 
-const handleLoadMore = () => {
-  if (after) {
-    fetchNew(after);
-  }
-};
+  const handleLoadMore = () => {
+    if (after) {
+      fetchData(after);
+    }
+  };
 
   return (
     <>
@@ -154,33 +84,13 @@ const handleLoadMore = () => {
         onNewsChange={handleNewsChange}
       />
 
-      {best === true &&
-        <FlatList
-          data={subredditsData}
-          renderItem={({ item }) => <Card subredditsData={item} />}
-          keyExtractor={(item, index) => index.toString()}
-          onEndReached={handleLoadMore}
-          onEndReachedThreshold={0.4}
-        />
-      }
-      {hot === true &&
-        <FlatList
-          data={subredditsData}
-          renderItem={({ item }) => <Card subredditsData={item} />}
-          keyExtractor={(item, index) => index.toString()}
-          onEndReached={handleLoadMore}
-          onEndReachedThreshold={0.4}
-        />
-      }
-      {news === true &&
-        <FlatList
-          data={subredditsData}
-          renderItem={({ item }) => <Card subredditsData={item} />}
-          keyExtractor={(item, index) => index.toString()}
-          onEndReached={handleLoadMore}
-          onEndReachedThreshold={0.4}
-        />
-      }
+      <FlatList
+        data={subredditsData}
+        renderItem={({ item }) => <Card subredditsData={item} />}
+        keyExtractor={(item, index) => index.toString()}
+        onEndReached={handleLoadMore}
+        onEndReachedThreshold={0.4}
+      />
     </>
   );
 }
--- components/VoteButtons.js
+++ components/VoteButtons.js
@@ -1,112 +1,120 @@
-import * as React from 'react';
-import { useState } from 'react';
-import { StyleSheet } from 'react-native';
-import { Card, Text, IconButton } from 'react-native-paper';
-import axios from 'axios';
+import * as React from "react";
+import { useState } from "react";
+import { StyleSheet } from "react-native";
+import { Card, Text, IconButton } from "react-native-paper";
+import axios from "axios";
 
 const MyComponent = ({ subredditsData }) => {
   const [upVoted, setUpVoted] = useState(false);
   const [downVoted, setDownVoted] = useState(false);
 
   const upVote = () => {
-    const token = 'eyJhbGciOiJSUzI1NiIsImtpZCI6IlNIQTI1NjphVXJUQUUrdnZWVTl4K0VMWFNGWEcrNk5WS1FlbEdtSjlWMkQxcWlCZ3VnIiwidHlwIjoiSldUIn0.eyJzdWIiOiJ1c2VyIiwiZXhwIjoxNjg0NDk0NzE3LCJpYXQiOjE2ODQ0MDgzMTcsImp0aSI6IjMxOTc5NTQ5MTc4OTI5LVBjRm5mR0ZGa0dFd1Y5S0ZxMHZ5Q2RhRVc4eTZ2USIsImNpZCI6ImlBanJEc0x5Rl9FU3RHSWFONDVOZ1EiLCJsaWQiOiJ0Ml9iYzM3OXc2NXQiLCJhaWQiOiJ0Ml9iYzM3OXc2NXQiLCJsY2EiOjE2ODQxNDg3ODc0MjAsInNjcCI6ImVKeUtWdEpTaWdVRUFBRF9fd056QVNjIiwiZmxvIjo5fQ.4dUyoj3zJOQ1YNStigFLeuFg5vyDarjjCClVMNcmhe2c36OWkNXc3cqtu8FRZs-jJnhkYgpOAN4V-B1fJ6Gr8kHVz41XDVe431K_QjlpT8r38ngQJ-nDvw1DkISAa_fKHDsiixwEHxOiaPaPnCL-Ly5Wz-83ZqhgLPsbLq0Jc_SVXkuIP-EBx3P2OqhDEFAS7I-ayLNV4KaKbib2xuHyfIht10DeJeCl4zNiM4O1abyzwALnEUP5AbiW4jACRcqbnFGvNh_UoIC4wuapWSptMxdTUJE4b6g3TKqVfJ5a9z8Lw4AOfTZ-8Iqrs7E8cJMv5TJExF1gQyGwUFdShaYxEg';
-    const id=subredditsData.data.name
-    const dir=1
+    const token =
+      "eyJhbGciOiJSUzI1NiIsImtpZCI6IlNIQTI1NjphVXJUQUUrdnZWVTl4K0VMWFNGWEcrNk5WS1FlbEdtSjlWMkQxcWlCZ3VnIiwidHlwIjoiSldUIn0.eyJzdWIiOiJ1c2VyIiwiZXhwIjoxNjg0NTY4OTQ1Ljc2NDAzMywiaWF0IjoxNjg0NDgyNTQ1Ljc2NDAzMiwianRpIjoiMzAyODI3NDgzMjM4LUNKYVhYZ0FsYkpiekJrR1lsMGdoYTItM1V1bVNtUSIsImNpZCI6IkxtS1kyQmJGSnNwaDNmZ29oZXUwTXciLCJsaWQiOiJ0Ml8zdjQ3bnhpZSIsImFpZCI6InQyXzN2NDdueGllIiwibGNhIjoxNTU5Mjc3MDQyMTE3LCJzY3AiOiJlSnlLVnRKU2lnVUVBQURfX3dOekFTYyIsImZsbyI6OX0.mhCVuz_k56qUlB5iaXVTEMy76ba6jwxlnnOaJVwVfdvd_cI8Cd7UbK_LdksPhUBlWswJV-rgowPHTLvATAnCXerhHyX-oFW8wNwrjVVvKuhMTwjFCJpLv3rB40gz7oBfqgqiShmwmyf54koET8GzY7aqZeb6fIXFjPmjXrVZl1iG3iXFjNtyz7jG0dbjlyflfQzs7dR2QfIuywmx0LahZyEkXwIoZd9NnFSVbVzfxwEh2LtKp1nN4PNq0QlukcM7AbQeCQEVQQU7svcUvj_VzNp9_FUWGo-p-b3NDT36Pfj4FMM_jIdocXdEZ_Xq1VFp43kqWYKVy95AHX-cttRwOQ";
+    const id = subredditsData.data.name;
+    const dir = 1;
 
     const options = {
-      method: 'POST',
-      url: 'https://oauth.reddit.com/api/vote',
+      method: "POST",
+      url: "https://oauth.reddit.com/api/vote",
       headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Authorization': `Bearer ${token}`,
+        "Content-Type": "application/x-www-form-urlencoded",
+        Authorization: `Bearer ${token}`,
       },
       data: `id=${id}&dir=${dir}`,
     };
-  
+
     if (upVoted === false) {
-      axios.request(options)
-      .then(response => {
-          setUpVoted(true)
+      axios
+        .request(options)
+        .then((response) => {
+          setUpVoted(true);
           console.log(response.data);
-          console.log("upVote successfull", subredditsData.data.score +1);
+          console.log("upVote successfull", subredditsData.data.score + 1);
         })
-        .catch(error => {
+        .catch((error) => {
           console.error(error);
         });
     } else if (upVoted === true) {
-      const token = 'eyJhbGciOiJSUzI1NiIsImtpZCI6IlNIQTI1NjphVXJUQUUrdnZWVTl4K0VMWFNGWEcrNk5WS1FlbEdtSjlWMkQxcWlCZ3VnIiwidHlwIjoiSldUIn0.eyJzdWIiOiJ1c2VyIiwiZXhwIjoxNjg0NDk0NzE3LCJpYXQiOjE2ODQ0MDgzMTcsImp0aSI6IjMxOTc5NTQ5MTc4OTI5LVBjRm5mR0ZGa0dFd1Y5S0ZxMHZ5Q2RhRVc4eTZ2USIsImNpZCI6ImlBanJEc0x5Rl9FU3RHSWFONDVOZ1EiLCJsaWQiOiJ0Ml9iYzM3OXc2NXQiLCJhaWQiOiJ0Ml9iYzM3OXc2NXQiLCJsY2EiOjE2ODQxNDg3ODc0MjAsInNjcCI6ImVKeUtWdEpTaWdVRUFBRF9fd056QVNjIiwiZmxvIjo5fQ.4dUyoj3zJOQ1YNStigFLeuFg5vyDarjjCClVMNcmhe2c36OWkNXc3cqtu8FRZs-jJnhkYgpOAN4V-B1fJ6Gr8kHVz41XDVe431K_QjlpT8r38ngQJ-nDvw1DkISAa_fKHDsiixwEHxOiaPaPnCL-Ly5Wz-83ZqhgLPsbLq0Jc_SVXkuIP-EBx3P2OqhDEFAS7I-ayLNV4KaKbib2xuHyfIht10DeJeCl4zNiM4O1abyzwALnEUP5AbiW4jACRcqbnFGvNh_UoIC4wuapWSptMxdTUJE4b6g3TKqVfJ5a9z8Lw4AOfTZ-8Iqrs7E8cJMv5TJExF1gQyGwUFdShaYxEg';
-      const id=subredditsData.data.name
-      const dir=0
-  
+      const token =
+        "eyJhbGciOiJSUzI1NiIsImtpZCI6IlNIQTI1NjphVXJUQUUrdnZWVTl4K0VMWFNGWEcrNk5WS1FlbEdtSjlWMkQxcWlCZ3VnIiwidHlwIjoiSldUIn0.eyJzdWIiOiJ1c2VyIiwiZXhwIjoxNjg0NTY4OTQ1Ljc2NDAzMywiaWF0IjoxNjg0NDgyNTQ1Ljc2NDAzMiwianRpIjoiMzAyODI3NDgzMjM4LUNKYVhYZ0FsYkpiekJrR1lsMGdoYTItM1V1bVNtUSIsImNpZCI6IkxtS1kyQmJGSnNwaDNmZ29oZXUwTXciLCJsaWQiOiJ0Ml8zdjQ3bnhpZSIsImFpZCI6InQyXzN2NDdueGllIiwibGNhIjoxNTU5Mjc3MDQyMTE3LCJzY3AiOiJlSnlLVnRKU2lnVUVBQURfX3dOekFTYyIsImZsbyI6OX0.mhCVuz_k56qUlB5iaXVTEMy76ba6jwxlnnOaJVwVfdvd_cI8Cd7UbK_LdksPhUBlWswJV-rgowPHTLvATAnCXerhHyX-oFW8wNwrjVVvKuhMTwjFCJpLv3rB40gz7oBfqgqiShmwmyf54koET8GzY7aqZeb6fIXFjPmjXrVZl1iG3iXFjNtyz7jG0dbjlyflfQzs7dR2QfIuywmx0LahZyEkXwIoZd9NnFSVbVzfxwEh2LtKp1nN4PNq0QlukcM7AbQeCQEVQQU7svcUvj_VzNp9_FUWGo-p-b3NDT36Pfj4FMM_jIdocXdEZ_Xq1VFp43kqWYKVy95AHX-cttRwOQ";
+      const id = subredditsData.data.name;
+      const dir = 0;
+
       const options = {
-        method: 'POST',
-        url: 'https://oauth.reddit.com/api/vote',
+        method: "POST",
+        url: "https://oauth.reddit.com/api/vote",
         headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-          'Authorization': `Bearer ${token}`,
+          "Content-Type": "application/x-www-form-urlencoded",
+          Authorization: `Bearer ${token}`,
         },
         data: `id=${id}&dir=${dir}`,
       };
 
-      axios.request(options)
-        .then(response => {
+      axios
+        .request(options)
+        .then((response) => {
           console.log(response.data);
           setUpVoted(false);
           console.log("Unvote successfull", subredditsData.data.score);
         })
-        .catch(error => {
+        .catch((error) => {
           console.error(error);
         });
     }
   };
 
   const downVote = () => {
-    const token = 'eyJhbGciOiJSUzI1NiIsImtpZCI6IlNIQTI1NjphVXJUQUUrdnZWVTl4K0VMWFNGWEcrNk5WS1FlbEdtSjlWMkQxcWlCZ3VnIiwidHlwIjoiSldUIn0.eyJzdWIiOiJ1c2VyIiwiZXhwIjoxNjg0NDk0NzE3LCJpYXQiOjE2ODQ0MDgzMTcsImp0aSI6IjMxOTc5NTQ5MTc4OTI5LVBjRm5mR0ZGa0dFd1Y5S0ZxMHZ5Q2RhRVc4eTZ2USIsImNpZCI6ImlBanJEc0x5Rl9FU3RHSWFONDVOZ1EiLCJsaWQiOiJ0Ml9iYzM3OXc2NXQiLCJhaWQiOiJ0Ml9iYzM3OXc2NXQiLCJsY2EiOjE2ODQxNDg3ODc0MjAsInNjcCI6ImVKeUtWdEpTaWdVRUFBRF9fd056QVNjIiwiZmxvIjo5fQ.4dUyoj3zJOQ1YNStigFLeuFg5vyDarjjCClVMNcmhe2c36OWkNXc3cqtu8FRZs-jJnhkYgpOAN4V-B1fJ6Gr8kHVz41XDVe431K_QjlpT8r38ngQJ-nDvw1DkISAa_fKHDsiixwEHxOiaPaPnCL-Ly5Wz-83ZqhgLPsbLq0Jc_SVXkuIP-EBx3P2OqhDEFAS7I-ayLNV4KaKbib2xuHyfIht10DeJeCl4zNiM4O1abyzwALnEUP5AbiW4jACRcqbnFGvNh_UoIC4wuapWSptMxdTUJE4b6g3TKqVfJ5a9z8Lw4AOfTZ-8Iqrs7E8cJMv5TJExF1gQyGwUFdShaYxEg';
-    const id=subredditsData.data.name
-    const dir=-1
+    const token =
+      "eyJhbGciOiJSUzI1NiIsImtpZCI6IlNIQTI1NjphVXJUQUUrdnZWVTl4K0VMWFNGWEcrNk5WS1FlbEdtSjlWMkQxcWlCZ3VnIiwidHlwIjoiSldUIn0.eyJzdWIiOiJ1c2VyIiwiZXhwIjoxNjg0NTY4OTQ1Ljc2NDAzMywiaWF0IjoxNjg0NDgyNTQ1Ljc2NDAzMiwianRpIjoiMzAyODI3NDgzMjM4LUNKYVhYZ0FsYkpiekJrR1lsMGdoYTItM1V1bVNtUSIsImNpZCI6IkxtS1kyQmJGSnNwaDNmZ29oZXUwTXciLCJsaWQiOiJ0Ml8zdjQ3bnhpZSIsImFpZCI6InQyXzN2NDdueGllIiwibGNhIjoxNTU5Mjc3MDQyMTE3LCJzY3AiOiJlSnlLVnRKU2lnVUVBQURfX3dOekFTYyIsImZsbyI6OX0.mhCVuz_k56qUlB5iaXVTEMy76ba6jwxlnnOaJVwVfdvd_cI8Cd7UbK_LdksPhUBlWswJV-rgowPHTLvATAnCXerhHyX-oFW8wNwrjVVvKuhMTwjFCJpLv3rB40gz7oBfqgqiShmwmyf54koET8GzY7aqZeb6fIXFjPmjXrVZl1iG3iXFjNtyz7jG0dbjlyflfQzs7dR2QfIuywmx0LahZyEkXwIoZd9NnFSVbVzfxwEh2LtKp1nN4PNq0QlukcM7AbQeCQEVQQU7svcUvj_VzNp9_FUWGo-p-b3NDT36Pfj4FMM_jIdocXdEZ_Xq1VFp43kqWYKVy95AHX-cttRwOQ";
+    const id = subredditsData.data.name;
+    const dir = -1;
 
     const options = {
-      method: 'POST',
-      url: 'https://oauth.reddit.com/api/vote',
+      method: "POST",
+      url: "https://oauth.reddit.com/api/vote",
       headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Authorization': `Bearer ${token}`,
+        "Content-Type": "application/x-www-form-urlencoded",
+        Authorization: `Bearer ${token}`,
       },
       data: `id=${id}&dir=${dir}`,
     };
-  
+
     if (downVoted === false) {
-      axios.request(options)
-        .then(response => {
+      axios
+        .request(options)
+        .then((response) => {
           console.log(response.data);
-          setDownVoted(true)
-          console.log("downVote successfull", subredditsData.data.score -1);
+          setDownVoted(true);
+          console.log("downVote successfull", subredditsData.data.score - 1);
         })
-        .catch(error => {
+        .catch((error) => {
           console.error(error);
         });
     } else if (downVoted === true) {
-      const token = 'eyJhbGciOiJSUzI1NiIsImtpZCI6IlNIQTI1NjphVXJUQUUrdnZWVTl4K0VMWFNGWEcrNk5WS1FlbEdtSjlWMkQxcWlCZ3VnIiwidHlwIjoiSldUIn0.eyJzdWIiOiJ1c2VyIiwiZXhwIjoxNjg0NDk0NzE3LCJpYXQiOjE2ODQ0MDgzMTcsImp0aSI6IjMxOTc5NTQ5MTc4OTI5LVBjRm5mR0ZGa0dFd1Y5S0ZxMHZ5Q2RhRVc4eTZ2USIsImNpZCI6ImlBanJEc0x5Rl9FU3RHSWFONDVOZ1EiLCJsaWQiOiJ0Ml9iYzM3OXc2NXQiLCJhaWQiOiJ0Ml9iYzM3OXc2NXQiLCJsY2EiOjE2ODQxNDg3ODc0MjAsInNjcCI6ImVKeUtWdEpTaWdVRUFBRF9fd056QVNjIiwiZmxvIjo5fQ.4dUyoj3zJOQ1YNStigFLeuFg5vyDarjjCClVMNcmhe2c36OWkNXc3cqtu8FRZs-jJnhkYgpOAN4V-B1fJ6Gr8kHVz41XDVe431K_QjlpT8r38ngQJ-nDvw1DkISAa_fKHDsiixwEHxOiaPaPnCL-Ly5Wz-83ZqhgLPsbLq0Jc_SVXkuIP-EBx3P2OqhDEFAS7I-ayLNV4KaKbib2xuHyfIht10DeJeCl4zNiM4O1abyzwALnEUP5AbiW4jACRcqbnFGvNh_UoIC4wuapWSptMxdTUJE4b6g3TKqVfJ5a9z8Lw4AOfTZ-8Iqrs7E8cJMv5TJExF1gQyGwUFdShaYxEg';
-      const id=subredditsData.data.name
-      const dir=0
-  
+      const token =
+        "eyJhbGciOiJSUzI1NiIsImtpZCI6IlNIQTI1NjphVXJUQUUrdnZWVTl4K0VMWFNGWEcrNk5WS1FlbEdtSjlWMkQxcWlCZ3VnIiwidHlwIjoiSldUIn0.eyJzdWIiOiJ1c2VyIiwiZXhwIjoxNjg0NTY4OTQ1Ljc2NDAzMywiaWF0IjoxNjg0NDgyNTQ1Ljc2NDAzMiwianRpIjoiMzAyODI3NDgzMjM4LUNKYVhYZ0FsYkpiekJrR1lsMGdoYTItM1V1bVNtUSIsImNpZCI6IkxtS1kyQmJGSnNwaDNmZ29oZXUwTXciLCJsaWQiOiJ0Ml8zdjQ3bnhpZSIsImFpZCI6InQyXzN2NDdueGllIiwibGNhIjoxNTU5Mjc3MDQyMTE3LCJzY3AiOiJlSnlLVnRKU2lnVUVBQURfX3dOekFTYyIsImZsbyI6OX0.mhCVuz_k56qUlB5iaXVTEMy76ba6jwxlnnOaJVwVfdvd_cI8Cd7UbK_LdksPhUBlWswJV-rgowPHTLvATAnCXerhHyX-oFW8wNwrjVVvKuhMTwjFCJpLv3rB40gz7oBfqgqiShmwmyf54koET8GzY7aqZeb6fIXFjPmjXrVZl1iG3iXFjNtyz7jG0dbjlyflfQzs7dR2QfIuywmx0LahZyEkXwIoZd9NnFSVbVzfxwEh2LtKp1nN4PNq0QlukcM7AbQeCQEVQQU7svcUvj_VzNp9_FUWGo-p-b3NDT36Pfj4FMM_jIdocXdEZ_Xq1VFp43kqWYKVy95AHX-cttRwOQ";
+      const id = subredditsData.data.name;
+      const dir = 0;
+
       const options = {
-        method: 'POST',
-        url: 'https://oauth.reddit.com/api/vote',
+        method: "POST",
+        url: "https://oauth.reddit.com/api/vote",
         headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-          'Authorization': `Bearer ${token}`,
+          "Content-Type": "application/x-www-form-urlencoded",
+          Authorization: `Bearer ${token}`,
         },
         data: `id=${id}&dir=${dir}`,
       };
 
-      axios.request(options)
-        .then(response => {
+      axios
+        .request(options)
+        .then((response) => {
           setDownVoted(false);
           console.log(response.data);
           console.log("UnDownVote successfull", subredditsData.data.score);
         })
-        .catch(error => {
+        .catch((error) => {
           console.error(error);
         });
     }
@@ -114,54 +122,41 @@ const MyComponent = ({ subredditsData }) => {
 
   return (
     <Card.Actions>
+      {upVoted === true && <IconButton icon="chevron-double-down" size={20} />}
+      {upVoted === false && (
+        <IconButton
+          icon="chevron-double-down"
+          size={20}
+          onPress={() => downVote()}
+        />
+      )}
 
-        {upVoted === true &&
-          <IconButton
-            icon="chevron-double-down"
-            size={20}
-          />
-        }
-        {upVoted === false &&
-          <IconButton
-            icon="chevron-double-down"
-            size={20}
-            onPress={() => downVote()}
-          />
-        }
+      {upVoted === false && downVoted === false && (
+        <Text variant="titleSmall">{subredditsData.data.score}</Text>
+      )}
 
-        {upVoted === false && downVoted === false &&
-          <Text variant="titleSmall">{subredditsData.data.score}</Text>
-        }
+      {upVoted === true && downVoted === false && (
+        <Text variant="titleSmall">{subredditsData.data.score + 1}</Text>
+      )}
 
-        {upVoted === true && downVoted === false &&
-          <Text variant="titleSmall">{subredditsData.data.score + 1}</Text>
-        }
-        
-        {downVoted === true && upVoted === false &&
-          <Text variant="titleSmall">{subredditsData.data.score - 1}</Text>
-        }
+      {downVoted === true && upVoted === false && (
+        <Text variant="titleSmall">{subredditsData.data.score - 1}</Text>
+      )}
 
-
-        {downVoted === true &&
-          <IconButton
-            mode='outlined'
-            icon="chevron-double-up"
-            size={20}
-          />
-        }
-        {downVoted === false &&
-          <IconButton
-            icon="chevron-double-up"
-            size={20}
-            onPress={() => upVote()}
-          />
-        }
-
+      {downVoted === true && (
+        <IconButton mode="outlined" icon="chevron-double-up" size={20} />
+      )}
+      {downVoted === false && (
+        <IconButton
+          icon="chevron-double-up"
+          size={20}
+          onPress={() => upVote()}
+        />
+      )}
     </Card.Actions>
-  )
+  );
 };
 
-const styles = StyleSheet.create({
-});
+const styles = StyleSheet.create({});
 
 export default MyComponent;
